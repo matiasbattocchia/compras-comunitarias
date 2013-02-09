6aae069eeb972b6bc365ed75be97b45c7465e6f0
Juan Ignacio Iglesias <iglesias.juanignacio@gmail.com>
2013-02-09 15:23:35 -0300

    Added URL to model so we can work with them individually.

--- client/js/models/Product.js
+++ client/js/models/Product.js
@@ -2,6 +2,10 @@ define (function (require, exports, module) {
     var BB = require('backbone');
 
     var ProductModel = BB.Model.extend({
+        url: function () {
+            return this.isNew() ? 'products.json'
+                                : 'products/' + this.get('id') + '.json';
+        }
     });
 
     return ProductModel;
